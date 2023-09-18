dbd75f567edc33852cb8e7a7bc243e60e2bf6d11
emeline <emeline.laigle@unicaen.fr>
2023-09-18 14:53:49 +0200

    :docs: ajouts des commentaires

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+ // Récupération des éléments HTML par leurs IDs
+ 
  let valeurTravail=document.getElementById("valeurtravail");
  let valeurPause=document.getElementById("valeurpause");
  let tempsTravail=valeurTravail.value;
@@ -8,7 +10,7 @@
  let boutonTravail=document.getElementById("travail");
  let boutonPause=document.getElementById("pause");
 
-
+// Rechargement de la page lorsque les champs de saisie sont cliqués
 
  valeurTravail.addEventListener('click',()=>{
     location.reload();
@@ -21,23 +23,27 @@
  })
 
 
+ // Gestion du clic sur le bouton "Start"
+
  boutonStart.addEventListener('click',()=>{
     demarage();
  });
+
+ // Gestion du clic sur le bouton "Reset" (rechargement de la page)
 
  boutonReset.addEventListener('click',()=>{
     location.reload();
  })
 
 
- //fonction ajoute les zéro devant 
+ // Fonction qui ajoute un zéro devant les nombres < 10
 
  function affichagePropre(temps){
     let tempsString=temps.toString();
     tempsString = tempsString.length < 2 ? '0' + tempsString : tempsString;
     return tempsString;
  }
- //affichage
+// Action lors du chargement de la page
 
  window.onload= ()=>{
     document.getElementById("minutes").textContent= affichagePropre(tempsTravail);
@@ -45,18 +51,18 @@
     boutonTravail.classList.add('active');
  }
 
- //fonction start 
+// Fonction de démarrage du minuteur
 
  function demarage(){
-    boutonStart.style.display="none";
-    boutonReset.style.display="block";
+    boutonStart.style.display="none"; // Masque le bouton "Start"
+    boutonReset.style.display="block";// Affiche le bouton "Reset"
 
-    secondes=59;
+    secondes=59; // Initialisation des secondes à 59
 
     let tempsMinutes=tempsTravail-1;
     let pauseMinutes=tempsPause-1;
 
-    comptepause=0;
+    comptepause=0; 
 
     let timerFunction=()=>{
         document.getElementById("minutes").textContent=affichagePropre(tempsMinutes);
@@ -70,23 +76,23 @@
                 if(comptepause%2==0){
                     tempsMinutes=pauseMinutes;
                     comptepause=comptepause+1;
-                    boutonTravail.classList.remove('active');
-                    boutonPause.classList.add('active');
-                    document.body.style.backgroundColor= "rgb(50,205,50)";
+                    boutonTravail.classList.remove('active'); // Désactive le bouton "Travail"
+                    boutonPause.classList.add('active'); // Active le bouton "Pause"
+                    document.body.style.backgroundColor= "rgb(50,205,50)"; // Change la couleur de fond
                 }
                 else{
                     tempsMinutes=tempsTravail-1;
                     comptepause=comptepause+1;
-                    boutonTravail.classList.add('active');
-                    boutonPause.classList.remove('active');
-                    document.body.style.backgroundColor= "#DC143C";
+                    boutonTravail.classList.add('active'); // Active le bouton "Travail"
+                    boutonPause.classList.remove('active'); // Désactive le bouton "Pause"
+                    document.body.style.backgroundColor= "#DC143C"; // Change la couleur de fond
                 }
             }
             secondes=59;
         }
     }
 
-    //Répétition de la fonction pour le décompte
+    // Répétition de la fonction pour le décompte toutes les 1000 ms (1 seconde)
     setInterval(timerFunction, 1000);
 
  }
